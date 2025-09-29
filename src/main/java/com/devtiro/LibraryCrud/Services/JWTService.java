422a6ff2564edfe2379d851fe0397afe306de4a9
Sinan <sinanikleriya18@gmail.com>
2025-09-29 22:01:34 +0100

    first commit

--- src/main/java/com/devtiro/LibraryCrud/Services/JWTService.java
+++ src/main/java/com/devtiro/LibraryCrud/Services/JWTService.java
@@ -54,7 +54,6 @@ public class JWTService {
     }
 
     public String extractUserName(String token) {
-        // extract the username from jwt token
         return extractClaim(token, Claims::getSubject);
     }
 
